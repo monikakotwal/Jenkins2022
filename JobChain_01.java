9430d483b3b035ab890ff3db7d778b151a67e400
monika <monikakotwal1990@gmail.com>
2023-02-23 22:57:40 +0530

    adding job chaining code

---
+++ JobChain_01.java
@@ -0,0 +1,8 @@
+public class JobChain_01 {
+
+	public static void main(String[] args) {
+		System.out.println("This is the parent job chaining code");
+
+	}
+
+}
---
+++ JobChain_02.java
@@ -0,0 +1,8 @@
+public class JobChain_02 {
+
+	public static void main(String[] args) {
+		System.out.println("This is child job for chaining code");
+
+	}
+
+}
