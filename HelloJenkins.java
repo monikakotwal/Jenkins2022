cbc18cce0a39460245c203b84b9fc0250c649663
monika <monikakotwal1990@gmail.com>
2023-02-23 21:31:29 +0530

    commitong hello jenkins code

---
+++ HelloJenkins.java
@@ -0,0 +1,9 @@
+public class HelloJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my first file in jenkins program");
+
+	}
+
+}
